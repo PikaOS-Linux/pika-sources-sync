e780b81a0daac3be6775b5f4cf7b1753e4463615
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-09-02 22:40:04 +0300

    Update amdgpu.sh

--- amdgpu.sh
+++ amdgpu.sh
@@ -32,7 +32,10 @@ mkdir -p ./output
 find . -name \*.deb -exec cp -vf {} ./output \;
 
 # Sign the packages
-mv -v ./output/*.deb ./output/*-$(date +"%Y%m%d")-.deb
+for i in ./output/*.deb
+do
+    mv -v $i $i-$(date +"%Y%m%d").deb
+done
 dpkg-sig --sign builder ./output/*.deb
 
 # Pull down existing ppa repo db files etc
