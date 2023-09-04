551143827579ed1a7e1ac4c60bb0e7a0cd63974e
ferrreo <harderthanfire@gmail.com>
2023-09-04 20:10:58 +0100

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -113,7 +113,7 @@ mkdir -p ./output
 find . -name \*.deb -exec cp -vf {} ./output \;
 
 # Sign the packages
-dpkg-sig --sign builder ./output/*.deb
+for f in ./output/*.deb; do dpkg-sig --sign builder "$f"; done
 
 # Pull down existing ppa repo db files etc
 rsync -azP --exclude '*.deb' ferreo@direct.pika-os.com:/srv/www/pikappa/ ./output/repo
@@ -131,7 +131,7 @@ wget -nv https://launchpad.net/ubuntu/+archive/primary/+files/reprepro_5.3.0-1.4
 apt install -y ./reprepro_5.3.0-1.4_amd64.deb
 
 # Add the new package to the repo
-reprepro -C ubuntu -V --basedir ./output/repo/ includedeb lunar ./output/*.deb
+for f in ./output/*.deb; do reprepro -C ubuntu -V --basedir ./output/repo/ includedeb lunar ./output/*.deb "$f"; done
 
 # Push the updated ppa repo to the server
 rsync -azP ./output/repo/ ferreo@direct.pika-os.com:/srv/www/pikappa/
