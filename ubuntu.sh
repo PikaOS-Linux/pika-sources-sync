ac7142f5dbd46ba344e69d6e75cfb69166f7040b
ferrreo <harderthanfire@gmail.com>
2023-09-05 01:33:45 +0100

    make ppp sign and add to repo as well

--- ubuntu.sh
+++ ubuntu.sh
@@ -38,7 +38,7 @@ rm -rfv ./*all.deb
 cd ../
 
 # Sign the packages
-for f in ./output/*.deb; do dpkg-sig --sign builder "$f"; done
+../ppp sign ./output/
 
 # Pull down existing ppa repo db files etc
 rsync -azP --exclude '*.deb' ferreo@direct.pika-os.com:/srv/www/pikappa/ ./output/repo
@@ -56,7 +56,7 @@ wget -nv https://launchpad.net/ubuntu/+archive/primary/+files/reprepro_5.3.0-1.4
 apt install -y ./reprepro_5.3.0-1.4_amd64.deb
 
 # Add the new package to the repo
-for f in ./output/*.deb; do reprepro -C ubuntu -V --basedir ./output/repo/ includedeb lunar ./output/*.deb "$f"; done
+../ppp repoadd ./output/ "-C ubuntu -V --basedir ./output/repo/ includedeb lunar"
 
 # Push the updated ppa repo to the server
 rsync -azP ./output/repo/ ferreo@direct.pika-os.com:/srv/www/pikappa/
