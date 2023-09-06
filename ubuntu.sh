9eb9761035822f43ed3f359023a70adaf7b29cb6
ferrreo <harderthanfire@gmail.com>
2023-09-06 18:40:15 +0100

    aptly is pogly

--- ubuntu.sh
+++ ubuntu.sh
@@ -5,7 +5,7 @@ set -e
 chmod 755 ./ppp
 
 # output folders
-mkdir -p ./output/incoming
+mkdir -p ./output
 
 cd ./output
 
@@ -18,39 +18,40 @@ apt install wget rsync ssh -y
 
 # Get ubuntu main pool
 echo "Getting ubuntu main pool 32bit"
-../ppp https://ppa.pika-os.com/dists/lunar/ubuntu/binary-i386/Packages.gz http://archive.ubuntu.com/ubuntu/dists/lunar/main/binary-i386/Packages.xz http://archive.ubuntu.com/ubuntu/ ./incoming
+../ppp https://ppa.pika-os.com/dists/lunar/ubuntu/binary-i386/Packages.gz http://archive.ubuntu.com/ubuntu/dists/lunar/main/binary-i386/Packages.xz http://archive.ubuntu.com/ubuntu/ ./
 echo "Getting ubuntu main pool 64bit"
-../ppp https://ppa.pika-os.com/dists/lunar/ubuntu/binary-amd64/Packages.gz http://archive.ubuntu.com/ubuntu/dists/lunar/main/binary-amd64/Packages.xz http://archive.ubuntu.com/ubuntu/ ./incoming
+../ppp https://ppa.pika-os.com/dists/lunar/ubuntu/binary-amd64/Packages.gz http://archive.ubuntu.com/ubuntu/dists/lunar/main/binary-amd64/Packages.xz http://archive.ubuntu.com/ubuntu/ ./
 
 # Get ubuntu multiverse pool
 # echo "Getting ubuntu multiverse pool 32bit"
-# ../ppp https://ppa.pika-os.com/dists/lunar/ubuntu/binary-i386/Packages.gz http://archive.ubuntu.com/ubuntu/dists/lunar/multiverse/binary-i386/Packages.xz http://archive.ubuntu.com/ubuntu/ ./incoming
+# ../ppp https://ppa.pika-os.com/dists/lunar/ubuntu/binary-i386/Packages.gz http://archive.ubuntu.com/ubuntu/dists/lunar/multiverse/binary-i386/Packages.xz http://archive.ubuntu.com/ubuntu/ ./
 # echo "Getting ubuntu multiverse pool 64bit"
-# ../ppp https://ppa.pika-os.com/dists/lunar/ubuntu/binary-amd64/Packages.gz http://archive.ubuntu.com/ubuntu/dists/lunar/multiverse/binary-amd64/Packages.xz http://archive.ubuntu.com/ubuntu/ ./incoming
+# ../ppp https://ppa.pika-os.com/dists/lunar/ubuntu/binary-amd64/Packages.gz http://archive.ubuntu.com/ubuntu/dists/lunar/multiverse/binary-amd64/Packages.xz http://archive.ubuntu.com/ubuntu/ ./
 
 # Get ubuntu restricted pool
 # echo "Getting ubuntu restricted pool 32bit"
-# ../ppp https://ppa.pika-os.com/dists/lunar/ubuntu/binary-i386/Packages.gz http://archive.ubuntu.com/ubuntu/dists/lunar/restricted/binary-i386/Packages.xz http://archive.ubuntu.com/ubuntu/ ./incoming
+# ../ppp https://ppa.pika-os.com/dists/lunar/ubuntu/binary-i386/Packages.gz http://archive.ubuntu.com/ubuntu/dists/lunar/restricted/binary-i386/Packages.xz http://archive.ubuntu.com/ubuntu/ ./
 # echo "Getting ubuntu restricted pool 64bit"
-# ../ppp https://ppa.pika-os.com/dists/lunar/ubuntu/binary-amd64/Packages.gz http://archive.ubuntu.com/ubuntu/dists/lunar/restricted/binary-amd64/Packages.xz http://archive.ubuntu.com/ubuntu/ ./incoming
+# ../ppp https://ppa.pika-os.com/dists/lunar/ubuntu/binary-amd64/Packages.gz http://archive.ubuntu.com/ubuntu/dists/lunar/restricted/binary-amd64/Packages.xz http://archive.ubuntu.com/ubuntu/ ./
 
 # Get ubuntu universe pool
 # echo "Getting ubuntu universe pool 32bit"
-# ../ppp https://ppa.pika-os.com/dists/lunar/ubuntu/binary-i386/Packages.gz http://archive.ubuntu.com/ubuntu/dists/lunar/universe/binary-i386/Packages.xz http://archive.ubuntu.com/ubuntu/ ./incoming
+# ../ppp https://ppa.pika-os.com/dists/lunar/ubuntu/binary-i386/Packages.gz http://archive.ubuntu.com/ubuntu/dists/lunar/universe/binary-i386/Packages.xz http://archive.ubuntu.com/ubuntu/ ./
 # echo "Getting ubuntu universe pool 64bit"
-# ../ppp https://ppa.pika-os.com/dists/lunar/ubuntu/binary-amd64/Packages.gz http://archive.ubuntu.com/ubuntu/dists/lunar/universe/binary-amd64/Packages.xz http://archive.ubuntu.com/ubuntu/ ./incoming
+# ../ppp https://ppa.pika-os.com/dists/lunar/ubuntu/binary-amd64/Packages.gz http://archive.ubuntu.com/ubuntu/dists/lunar/universe/binary-amd64/Packages.xz http://archive.ubuntu.com/ubuntu/ ./
 
-# if [ $(ls ./incoming | wc -l) -lt 1 ]; then
-# echo "Repos are synced"
-#     exit 0
-# fi
+cd ../
+
+if [ $(ls ./output/ | wc -l) -lt 1 ]; then
+echo "Repos are synced"
+    exit 0
+fi
 
 # send debs to server
-rsync -azP ./incoming/ ferreo@direct.pika-os.com:/srv/www/incoming/
+rsync -azP ./output/ ferreo@direct.pika-os.com:/srv/www/incoming/
 
 # add debs to repo
 ssh ferreo@direct.pika-os.com 'aptly repo add -force-replace -remove-files pika-ubuntu /srv/www/incoming/'
 
 # publish the repo
-ssh ferreo@direct.pika-os.com 'aptly publish repo -batch -skip-contents -force-overwrite -component=,,,, pika-a
-mdgpu pika-external pika-main pika-rocm pika-ubuntu filesystem:pikarepo:'
+ssh ferreo@direct.pika-os.com 'aptly publish repo -batch -skip-contents -force-overwrite -component=,,,, pika-amdgpu pika-external pika-main pika-rocm pika-ubuntu filesystem:pikarepo:'
