ed1f89fe05e3b68a650394ec52b86858b9614038
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-09-18 18:29:23 +0300

    Update external-lunarg-vulkan.sh

--- external-lunarg-vulkan.sh
+++ external-lunarg-vulkan.sh
@@ -8,45 +8,15 @@ chmod 755 ./ppp
 mkdir -p ./output/lunarg
 cd ./output/lunarg
 
-# temp
-apt update
-apt upgrade -y
-# end of temp
+../ppp https://ppa.pika-os.com/dists/lunar/external/binary-i386/Packages https://packages.lunarg.com/vulkan/dists/jammy/main/binary-amd64/Packages https://packages.lunarg.com/vulkan/ ./
 
-#Get rid of Pika sources to prevent conflicts
-rm -rf /etc/apt/sources.list.d/pika*
-rm -rf  /etc/apt/preferences.d/*pika*
+../ppp https://ppa.pika-os.com/dists/lunar/external/binary-amd64/Packages https://packages.lunarg.com/vulkan/dists/jammy/main/binary-amd64/Packages https://packages.lunarg.com/vulkan/ ./
 
-# Get LunarG Vulkan SDK pool
-echo 'deb [rusted=yes] deb https://packages.lunarg.com/vulkan jammy main' | sudo tee /etc/apt/sources.list.d/lunarg-vulkan.list
-wget -qO- https://packages.lunarg.com/lunarg-signing-key-pub.asc | sudo tee /etc/apt/trusted.gpg.d/lunarg.asc
-apt update -y
-
-PPP32=$(../../ppp https://ppa.pika-os.com/dists/lunar/external/binary-i386/Packages https://packages.lunarg.com/vulkan/dists/jammy/main/binary-i386/Packages)
-if [ ! -z "$PPP32" ]
-then
-    dpkg --add-architecture i386
-    apt update -o APT::Architecture="i386" -o APT::Architectures="i386" -y --allow-unauthenticated 
-    apt download $PPP32 -o APT::Architecture="i386" -o APT::Architectures="i386" -y --target-release 'o=vulkan.lunarg.com'
-    rm -rfv ./*all.deb
-else
-    echo "i386 Repos are synced"
-fi
-
-PPP64=$(../../ppp https://ppa.pika-os.com/dists/lunar/external/binary-amd64/Packages https://packages.lunarg.com/vulkan/dists/jammy/main/binary-amd64/Packages)
-if [ ! -z "$PPP64" ]
-then
-    apt update -o APT::Architecture="amd64" -o APT::Architectures="amd64" -y --allow-unauthenticated 
-    apt download $PPP64 -o APT::Architecture="amd64" -o APT::Architectures="amd64" -y --target-release 'o=vulkan.lunarg.com'
-else
-    echo "AMD64 Repos are synced"
-    exit 0
-fi
-
-# Return to LunarG Vulkan SDK MIRROR
 cd ../
-mkdir -p ./output
-find . -name \*.deb -exec cp -vf {} ./output \;
+
+if [ $(ls ./output/ | wc -l) -lt 1 ]; then
+    echo "Lunar repos are synced"
+fi
 
 # send debs to server
 rsync -azP ./output/ ferreo@direct.pika-os.com:/srv/www/incoming/
