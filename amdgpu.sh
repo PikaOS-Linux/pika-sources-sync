6ef88fcc05b8ab0eea5590a194b94dda4b73d0ee
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-10-17 22:06:27 +0300

    Update amdgpu.sh

--- amdgpu.sh
+++ amdgpu.sh
@@ -7,37 +7,37 @@ cd ./outputpika/amdgpu
 # amdgpu drm dir
 mkdir -p ./libd
 cd ./libd
-wget --recursive --no-parent -R "*20.04*.deb" -A "*" -m http://repo.radeon.com/amdgpu/5.7/ubuntu/pool/main/libd/
+wget --recursive --no-parent -R "*20.04*.deb" -A "*" -m http://repo.radeon.com/amdgpu/5.7.1/ubuntu/pool/main/libd/
 # Return to AMDGPU MIRROR
 cd ../
 # amdgpu mesa dir (depends only)
 mkdir -p ./mesa
 cd ./mesa
-wget --recursive --no-parent -R "*20.04*.deb" -A "*" -m http://repo.radeon.com/amdgpu/5.7/ubuntu/pool/main/m/mesa-amdgpu/
+wget --recursive --no-parent -R "*20.04*.deb" -A "*" -m http://repo.radeon.com/amdgpu/5.7.1/ubuntu/pool/main/m/mesa-amdgpu/
 # Return to AMDGPU MIRROR
 cd ../
 # amdgpu wayland dir (depends only)
 mkdir -p ./wayland-amdgpu
 cd ./wayland-amdgpu
-wget --recursive --no-parent -R "*20.04*.deb" -A "*" -m http://repo.radeon.com/amdgpu/5.7/ubuntu/pool/main/w/wayland-amdgpu/
+wget --recursive --no-parent -R "*20.04*.deb" -A "*" -m http://repo.radeon.com/amdgpu/5.7.1/ubuntu/pool/main/w/wayland-amdgpu/
 # Return to AMDGPU MIRROR
 cd ../
 # amdgpu wayland protocols dir (depends only)
 mkdir -p ./wayland-protocols-amdgpu
 cd ./wayland-protocols-amdgpu
-wget --recursive --no-parent -R "*20.04*.deb" -A "*" -m http://repo.radeon.com/amdgpu/5.7/ubuntu/pool/main/w/wayland-protocols-amdgpu/
+wget --recursive --no-parent -R "*20.04*.deb" -A "*" -m http://repo.radeon.com/amdgpu/5.7.1/ubuntu/pool/main/w/wayland-protocols-amdgpu/
 # Return to AMDGPU MIRROR
 cd ../
 # amdgpu proprietary dir
 mkdir -p ./proprietary
 cd ./proprietary
-wget --recursive --no-parent -R "*20.04*.deb" -A "*" -m https://repo.radeon.com/amdgpu/5.7/ubuntu/pool/proprietary/
+wget --recursive --no-parent -R "*20.04*.deb" -A "*" -m https://repo.radeon.com/amdgpu/5.7.1/ubuntu/pool/proprietary/
 # Return to AMDGPU MIRROR
 cd ../
 # amdgpu-dkms-firmware dir
 mkdir -p ./amdgpu-dkms-firmware
 cd ./amdgpu-dkms-firmware
-wget http://repo.radeon.com/amdgpu/5.7/ubuntu/pool/main/a/amdgpu-dkms/amdgpu-dkms-firmware_6.2.4.50700-1652687.22.04_all.deb
+wget http://repo.radeon.com/amdgpu/5.7.1/ubuntu/pool/main/a/amdgpu-dkms/amdgpu-dkms-firmware_6.2.4.50701-1664922.22.04_all.deb
 # Return to AMDGPU MIRROR
 cd ../
 mkdir -p ./outputpika
