3810df105287e7a9578471933edd870a37fd0fc9
ferrreo <harderthanfire@gmail.com>
2023-10-26 20:14:52 +0100

    Update syncpurge.sh

--- syncpurge.sh
+++ syncpurge.sh
@@ -2,6 +2,6 @@
 set -e
 
 # publish all the repos
-ssh ferreo@direct.pika-os.com 'aptly publish update -batch -skip-contents -force-overwrite pikauwu filesystem:pikarepo:'
-
 ssh ferreo@direct.pika-os.com 'aptly publish update -batch -skip-contents -force-overwrite lunar filesystem:pikarepo:'
+
+ssh ferreo@direct.pika-os.com 'aptly publish update -batch -skip-contents -force-overwrite pikauwu filesystem:pikarepo:'
