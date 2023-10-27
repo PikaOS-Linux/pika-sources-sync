0156ee008a30e9e601f79299bc74033935d84183
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-10-27 12:10:46 +0300

    Update oneapi.sh

--- oneapi.sh
+++ oneapi.sh
@@ -10,9 +10,11 @@ cd ./manticoutput
 
 ../ppp  https://ppa.pika-os.com/dists/pikauwu/oneapi/binary-amd64/Packages https://repositories.intel.com/gpu/ubuntu/dists/jammy/unified/binary-amd64/Packages https://repositories.intel.com/gpu/ubuntu/ ./
 
-rm -rfv intel-gsc_*_amd64.deb 
-rm -rfv libdrm*.deb 
-rm -rfv libmetee*.deb
+rm -rfv 
+rm -rfv 
+rm -rfv 
+rm -rfv 
+rm -rfv
 
 cd ../
 
@@ -23,6 +25,13 @@ fi
 
 # send debs to server
 rsync -azP ./manticoutput/ ferreo@direct.pika-os.com:/srv/www/incoming/
+
+# Remove currently broken debs
+ssh ferreo@direct.pika-os.com 'rm -rfv /srv/www/incoming/intel-gsc_*_amd64.deb '
+ssh ferreo@direct.pika-os.com 'rm -rfv /srv/www/incoming/intel-gsc-dev_*_amd64.deb'
+ssh ferreo@direct.pika-os.com 'rm -rfv /srv/www/incoming/intel-i915-dkms_*.deb'
+ssh ferreo@direct.pika-os.com 'rm -rfv /srv/www/incoming/libdrm*.deb'
+ssh ferreo@direct.pika-os.com 'rm -rfv /srv/www/incoming/libmetee*.deb'
 
 # add debs to repo
 ssh ferreo@direct.pika-os.com 'aptly repo add -force-replace -remove-files pikauwu-oneapi /srv/www/incoming/'
