f7d0555ad262769931cc6d03c077e46572b68120
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-10-27 13:58:52 +0300

    Update oneapi.sh

--- oneapi.sh
+++ oneapi.sh
@@ -17,6 +17,10 @@ cd ./manticoutput-tmp
 
 ../ppp  https://ppa.pika-os.com/dists/pikauwu/rocm/binary-amd64/Packages https://repositories.intel.com/gpu/ubuntu/dists/jammy/unified/binary-amd64/Packages https://repositories.intel.com/gpu/ubuntu/ ./
 
+rm -rfv ./intel-i915-dkms_*.deb
+rm -rfv ./libdrm*.deb
+rm -rfv ./*va*.deb
+
 if [ $(ls ./ | wc -l) -lt 1 ]; then
     echo "Mantic repos are synced"
     exit 0
@@ -39,13 +43,6 @@ mv -v ./manticoutput-tmp/*-fixed.deb ./manticoutput/
 
 # send debs to server
 rsync -azP ./manticoutput/ ferreo@direct.pika-os.com:/srv/www/incoming/
-
-# Remove currently broken debs
-ssh ferreo@direct.pika-os.com 'rm -rfv /srv/www/incoming/intel-i915-dkms_*.deb'
-ssh ferreo@direct.pika-os.com 'rm -rfv /srv/www/incoming/libdrm*.deb'
-ssh ferreo@direct.pika-os.com  /bin/bash << EOF
-rm -rfv /srv/www/incoming/*va*.deb
-EOF
 
 # add debs to repo
 ssh ferreo@direct.pika-os.com 'aptly repo add -force-replace -remove-files pikauwu-rocm /srv/www/incoming/'
