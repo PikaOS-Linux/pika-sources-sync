e84ffe252785b63d3e449da5ae32cf094d0177bd
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-11-24 17:25:49 +0300

    Update amdgpu.sh

--- amdgpu.sh
+++ amdgpu.sh
@@ -37,7 +37,7 @@ cd ../
 # amdgpu-dkms-firmware dir
 mkdir -p ./amdgpu-dkms-firmware
 cd ./amdgpu-dkms-firmware
-wget http://repo.radeon.com/amdgpu/5.7.2/ubuntu/pool/main/a/amdgpu-dkms/amdgpu-dkms-headers_6.2.4.50702-1683306.22.04_all.deb
+wget http://repo.radeon.com/amdgpu/5.7.2/ubuntu/pool/main/a/amdgpu-dkms/amdgpu-dkms-firmware_6.2.4.50702-1683306.22.04_all.deb
 # Return to AMDGPU MIRROR
 cd ../
 mkdir -p ./outputpika
@@ -50,7 +50,7 @@ do
     mkdir $i-tmp
     dpkg-deb -R $i $i-tmp
     cat $i-tmp/DEBIAN/control | grep Version: | head -n1 | cut -d":" -f2- | tr -d ' ' > $i-version
-    sed -i "s#$(cat $i-version)#$(cat $i-version)-pika$(date +"%Y%m%d").pikauwu1#g" $i-tmp/DEBIAN/control
+    sed -i "s#$(cat $i-version)#$(cat $i-version)-pika$(date +"%Y%m%d").pikauwu2#g" $i-tmp/DEBIAN/control
     dpkg-deb -b $i-tmp $i-"$(date +"%Y%m%d")"-pika-pikauwu1-fixed.deb
 done
 cd ../
